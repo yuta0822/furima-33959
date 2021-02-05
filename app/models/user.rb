ecb09044c10e51416c4a7807fe3debe07b5722ce
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-06 02:22:47 +0900

    漢字カタカナひらがなの制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname,              presence: true
-  validates :family_name,           presence: true
-  validates :first_name,            presence: true
-  validates :family_name_kana,      presence: true
-  validates :first_name_kana,       presence: true
-  validates :birthdate,             presence: true
   
-    
+
+  with_options presence: true do
+    validates :nickname,              presence: true
+    validates :family_name,           format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
+    validates :first_name,            format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
+    validates :family_name_kana,      format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
+    validates :first_name_kana,       format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
+    validates :birthdate,             presence: true
+  end
 end
+  
