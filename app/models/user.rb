8eefa812bc0347306b2d15ec2a17f52e012489e1
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-07 20:28:35 +0900

    不要な記述とバリデーションのまとめ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-
   with_options presence: true do
-    validates :nickname,              presence: true
-    validates :family_name,           format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
-    validates :first_name,            format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
-    validates :family_name_kana,      format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
-    validates :first_name_kana,       format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
-    validates :birthdate,             presence: true
+    validates :nickname  
+    validates :birthdate 
   end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }  do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }do
+   validates :family_name_kana
+   validates :first_name_kana
+  end 
 end
-  
