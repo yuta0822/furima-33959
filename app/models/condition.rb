164454bf63804d5a5f2c250ba874cf38b1265140
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-08 18:30:43 +0900

    全体ActiveHash　修正

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,10 +1,14 @@
-class Postage < ActiveHash::Base
+class Condition < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '着払い（購入者負担）' },
-    { id: 3, name: '送料込み（出品者負担）' },
+    { id: 2, name: '新品、未使用' },
+    { id: 3, name: '未使用に近い' },
+    { id: 4, name: '目立った傷や汚れなし' },
+    { id: 5, name: 'やや傷や汚れあり' },
+    { id: 6, name: '傷や汚れあり' },
+    { id: 7, name: '全体的に状態が悪い' },
   ]
     include ActiveHash::Associations
     has_many :items
-
-  end 
+  
+end 
