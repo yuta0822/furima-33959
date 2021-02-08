b079ae01f5afeca7bdcc5b1639cda7d94052cf3f
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-08 15:12:55 +0900

    Itemモデルにバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,13 @@ class Item < ApplicationRecord
   belongs_to             :user
   has_one_attached       :image
 
+  validates :image,         presence: true
+  validates :name,          presence: true
+  validates :explanation,   presence: true
+  validates :condition_id,  presence: true 
+  validates :postage_id,    presence: true 
+  validates :prefecture_id, presence: true
+  validates :prepare_id,    presence: true
+  validates :price ,        presence: true
+
 end
