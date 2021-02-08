7e4ef7c56cf4759be0803bc17d922b675d5b2bc4
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-08 18:15:49 +0900

    ActiveHash　postage　定義つけ

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :postage
   
   belongs_to             :user
   has_one_attached       :image
@@ -11,7 +12,7 @@ class Item < ApplicationRecord
   validates :explanation,   presence: true
   validates :category_id,   numericality: { other_than: 1 } 
   validates :condition_id,  numericality: { other_than: 1 } 
-  validates :postage_id,    presence: true 
+  validates :postage_id,    numericality: { other_than: 1 }  
   validates :prefecture_id, presence: true
   validates :prepare_id,    presence: true
   validates :price ,        presence: true
