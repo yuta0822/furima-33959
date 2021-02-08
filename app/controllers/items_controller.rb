4a17cfc2b84d15c38954c0236e523f3ce1449045
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-08 21:50:04 +0900

    params.require(:item).permitにイメージとprepare_idを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id, :prepare_id, :price,:image).merge(user_id: current_user.id)
   end
 end
