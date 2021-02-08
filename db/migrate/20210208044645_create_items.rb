e4e48a803dc9255bb6e5c353782b90581c16bdaf
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-08 14:01:32 +0900

    itemデータベース反映

--- db/migrate/20210208044645_create_items.rb
+++ db/migrate/20210208044645_create_items.rb
@@ -9,7 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :postage_id,     null: false
       t.integer :prefecture_id,  null: false
       t.integer :price,          null: false
-      t.references :user,        foreign_key: true
+      t.references :user,         foreign_key: true
 
       t.timestamps
     end
