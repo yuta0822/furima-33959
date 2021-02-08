977494f9813f1f3491c81564dc680ce1218f2f62
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-08 14:07:31 +0900

    itemとUserモデルにアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  
+　belongs_to_active_hash :category
+　belongs_to_active_hash :prefecture
+　belongs_to_active_hash :condition
+　belongs_to_active_hash :postage
+　belongs_to_active_hash :prepare
+　belongs_to             :user
+
 end
