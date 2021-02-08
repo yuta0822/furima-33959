c4ef4a1acfba9cf702893f370a6b5777767e7234
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-08 18:05:01 +0900

    ActiveHash　category定義つけ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
   
   belongs_to             :user
   has_one_attached       :image
@@ -6,6 +8,7 @@ class Item < ApplicationRecord
   validates :image,         presence: true
   validates :name,          presence: true
   validates :explanation,   presence: true
+  validates :category_id,   numericality: { other_than: 1 } 
   validates :condition_id,  presence: true 
   validates :postage_id,    presence: true 
   validates :prefecture_id, presence: true
