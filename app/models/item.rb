854dc520cf5d112a89212a63b75cef0a83454306
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-08 18:10:02 +0900

    ActiveHash　condition　定義つけ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
   
   belongs_to             :user
   has_one_attached       :image
@@ -9,7 +10,7 @@ class Item < ApplicationRecord
   validates :name,          presence: true
   validates :explanation,   presence: true
   validates :category_id,   numericality: { other_than: 1 } 
-  validates :condition_id,  presence: true 
+  validates :condition_id,  numericality: { other_than: 1 } 
   validates :postage_id,    presence: true 
   validates :prefecture_id, presence: true
   validates :prepare_id,    presence: true
