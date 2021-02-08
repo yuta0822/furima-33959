61a7857ec245ad6e536cd74a205a8c6aebfd742a
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-08 15:19:08 +0900

    itemコントローラーにindex new create 記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
+
+
   def index
+    @item = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,5 @@
 class Item < ApplicationRecord
   
-  belongs_to_active_hash :category
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :postage
-  belongs_to_active_hash :prepare
   belongs_to             :user
   has_one_attached       :image
 
