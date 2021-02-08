0f2da8c639938bb4ded492fe34b82e6b660efb58
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-08 16:53:29 +0900

    createの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save
+       redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id, :price).merge(user_id: current_user.id)
+  end
 end
