d392566a62067e29989b0d180c68aaf868d97825
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-09 19:29:43 +0900

    不要な記述削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,5 +22,5 @@ class Item < ApplicationRecord
     validates :prepare_id
     end
   end
-  validates :price,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}, format:{with: /\A[0-9]+\z/}
+  validates :price,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
 end
