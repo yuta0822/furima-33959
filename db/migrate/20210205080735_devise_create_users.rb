9d1ed66858ab65c3fa71ca88ec2a4c0c87e1bc56
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-09 20:44:53 +0900

    test

--- db/migrate/20210205080735_devise_create_users.rb
+++ db/migrate/20210205080735_devise_create_users.rb
@@ -3,7 +3,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname,           null: false, unique: true
@@ -11,11 +10,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name,         null: false
       t.string :family_name_kana,   null: false
       t.string :first_name_kana,    null: false
-      t.date   :birthdate,          null: false
-
-
-      
-
+      # t.date   :birthdate,          null: false
+      ## Database authenticatable
+      # t.string :email,              null: false, default: ""
+      # t.string :encrypted_password, null: false, default: ""
+      # t.string :nickname,           null: false, unique: true
+      # t.string :family_name,        null: false
+      # t.date :birthdate, null: false
+      t.date :birthdate, null: false
+      # t.string :first_name,         null: false
+      # t.string :family_name_kana,   null: false
+      # t.string :first_name_kana,    null: false
+      # t.date :birthdate, null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -51,12 +51,12 @@ ActiveRecord::Schema.define(version: 2021_02_08_054424) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nickname", default: "", null: false
+    t.string "nickname", null: false
     t.string "family_name", null: false
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.date "birthdate"
+    t.date "birthdate", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
