07d9040409ca122392cd4af02e6c7439a367d774
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-10 17:52:53 +0900

    showルートとコントローラー、パス設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id, :prepare_id, :price,:image).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-  resources :items, only: [:new, :create, :index] do
+  resources :items, only: [:new, :create, :index, :show] do
   end
 
 end
