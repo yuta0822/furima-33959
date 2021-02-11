9eb30a43c7f5bb30bb5a3b4a38c68f6148941648
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-11 11:09:20 +0900

    updateと editルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-  resources :items, only: [:new, :create, :index, :show] do
+  resources :items, only: [:new, :create, :index, :show, :edit, :update] do
   end
 
 end
