8469c290d9fb933dad0c824456d6071abe443b10
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-11 11:27:09 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,14 +22,14 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
     unless @item.user_id == current_user.id
-      redirect_to prototypes_path
+      redirect_to items_path
     end
   end
 
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-       redirect_to prototype_path
+       redirect_to item_path
     else
       render :edit
     end
