c6e949fbf9cf214ea3cb5f64fab0f2e42c612c48
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-11 14:43:58 +0900

    before_action :move_to_index newの記述もれ訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new] 
  
 
   def index
