430182c647950122853395832c37e08084de6a36
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-11 14:17:46 +0900

    コメントビュー修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
+  before_action :move_to_index, except: [:index, :show]
+ 
 
   def index
     @items = Item.order("created_at DESC")
