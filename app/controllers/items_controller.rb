8994c5aa9103fe896a4e2c5bc1ec789008afb7c4
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-11 15:12:05 +0900

    destory パス、コントローラー記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
-  before_action :move_to_index, except: [:index, :show, :new, :create] 
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show, :new, :create,:destroy] 
  
 
   def index
@@ -36,6 +36,11 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   def set_item
