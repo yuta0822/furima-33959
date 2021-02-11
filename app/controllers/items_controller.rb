b8b02624b465924c791344b07f00fcd07604f515
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-11 14:48:49 +0900

    before_action :move_to_index creste記述追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
-  before_action :move_to_index, except: [:index, :show, :new] 
+  before_action :move_to_index, except: [:index, :show, :new, :create] 
  
 
   def index
