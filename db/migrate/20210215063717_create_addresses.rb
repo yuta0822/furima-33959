8ec3bcdf14ec7919a7cae50a8d603b70648fa097
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-15 16:35:40 +0900

    データベースの作成

--- db/migrate/20210215063717_create_addresses.rb
+++ db/migrate/20210215063717_create_addresses.rb
@@ -8,7 +8,6 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string :home_number,          null: false
       t.string :building_name
       t.string :phone_number,         null: false
-      t.string :post_code,            null: false
       t.references :order,             foreign_key: true
 
       t.timestamps
