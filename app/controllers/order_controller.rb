5e4267c3953e472a858eaaf23d3e5aa2fc1670f5
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-16 15:04:10 +0900

    order のコントローラーの定義（未完成）

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,2 +1,25 @@
 class OrderController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @order = Order.create(order_params)
+    Address.create(address_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order)permit(:item).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params.require(:address)permit(:post_code, :prefecture_id, :home_number, :building_name, :phone_number).merge(item_id: @item.id)
+  end
+
 end
