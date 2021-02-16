79cbd9b4f6af8f579d3d05018e19ef09d17af219
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-16 15:22:54 +0900

    order_addressモデル作成　バリデーション定義

---
+++ app/models/order_address.rb
@@ -0,0 +1,15 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :phone_number, :prefecture, :city, :house_number, :building_name, :post_code, :prefecture_id, :user_id, :item_id
+
+  with_options presence: true do
+    validates :city
+    validates :home_number
+    validates :phone_number
+  end
+  validates :post_code,        presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :building_name
+  validates :prefecture_id,    presence: true, numericality: { other_than: 1 }
+
+  
+end
