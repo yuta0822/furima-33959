6515159313abc83e831ed544b8e378e3c5c45fb7
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-16 11:27:15 +0900

    addressにバリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,15 @@
 class Address < ApplicationRecord
+
   belongs_to :order
+
+  with_options presence: true do
+    validates :post_code
+    validates :city
+    validates :home_number
+    validates :phone_number
+  end
+  
+  validates :building_name
+  validates :prefecture_id,    presence: true, numericality: { other_than: 1 }
+
 end
