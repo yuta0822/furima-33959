54fb7dd1d38b2b6a1b33a31d9a7aae4b6e53405d
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-16 15:23:25 +0900

    addressバリデーション記述削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,15 +1,4 @@
 class Address < ApplicationRecord
-
   belongs_to :order
-
-  with_options presence: true do
-    validates :city
-    validates :home_number
-    validates :phone_number
-  end
-
-  validates :post_code,        presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :building_name
-  validates :prefecture_id,    presence: true, numericality: { other_than: 1 }
 
 end
