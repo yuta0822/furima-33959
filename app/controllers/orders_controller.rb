fdfbe45c19828040d1a74b1a4d6f5be765d3d608
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-02-17 15:03:25 +0900

    クレジットカード機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,19 +6,29 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(address_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end 
 
   private
   def address_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :home_number, :building_name, :phone_number, :city).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:post_code, :prefecture_id, :home_number, :building_name, :phone_number, :city).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_c47dfc0be7871c73e5a4534d"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: address_params[:token],
+      currency: 'jpy' 
+    )
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :phone_number, :city, :home_number, :building_name, :post_code, :prefecture_id, :user_id, :item_id
+  attr_accessor :phone_number, :city, :home_number, :building_name, :post_code, :prefecture_id, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :city
@@ -8,12 +8,13 @@ class OrderAddress
     validates :phone_number,   format: {with: /\A\d{11}\z/, message: "Input only number"}
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :post_code,        presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id,    presence: true, numericality: { other_than: 1 }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(city: city, home_number: home_number, phone_number: phone_number, post_code: post_code, building_name: building_name, prefecture_id: prefecture_id, order_id: order.id)
+    Address.create(city: city, home_number: home_number, phone_number: phone_number, post_code: post_code, building_name: building_name, prefecture_id: prefecture_id, order_id: order.id )
   end
 end
